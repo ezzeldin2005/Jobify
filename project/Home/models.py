8f6e2e4a2ff153b38c591aeeb0d0387ebc2c8b18
Ziad Omar <61595704+Ezay06@users.noreply.github.com>
2025-05-11 21:06:52 +0300

    Fixed issues

--- project/Home/models.py
+++ project/Home/models.py
@@ -6,7 +6,6 @@ class AdminData(models.Model):
     Password = models.CharField(max_length= 20)
     Email = models.CharField(max_length= 40)
     CompanyName = models.CharField(max_length= 40)
-    ID = models.IntegerField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')
 
 class UserData(models.Model):
     Username = models.CharField(max_length= 40)
