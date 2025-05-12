f3a5d6c7dbc897b0e7706aa4fbf93aa6fd7f13ea
Ziad Omar <61595704+Ezay06@users.noreply.github.com>
2025-05-12 14:16:16 +0300

    fixed issue

--- project/Home/models.py
+++ project/Home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class AdminData(models.Model):
-    Username = models.CharField(max_length= 40)
+    Username = models.CharField(max_length= 40, unique = True)
     Password = models.CharField(max_length= 20)
     Email = models.CharField(max_length= 40)
     CompanyName = models.CharField(max_length= 40)
